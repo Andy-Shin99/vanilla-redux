b6f7289361fc7e11dd461a537166fede0e38d4ff
Shin Minkyu <tlsalsrb990713@gmail.com>
2023-07-23 17:30:06 +0900

    :sparkles: React todo list 기본 틀 작성

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,3 +1,25 @@
+import {useState} from "react";
+
 export default function Home() {
-  return "Home";
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText("");
+  };
+
+  const handleInputChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleInputChange} />
+        <button>ADD</button>
+      </form>
+      <ul></ul>
+    </>
+  );
 }
